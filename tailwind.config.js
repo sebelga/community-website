ab48633cb90b135e81a2840ca0c5ac487a567022
Sébastien Loix <sabee77@gmail.com>
2024-09-29 18:30:52 +0100

    Update repo folder structure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "dev/index.html",
+    "dev/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
